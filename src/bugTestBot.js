251362158f0c70a49b9c3676cdc5ccec429160ee
wakfi <55608093+wakfi@users.noreply.github.com>
2020-08-23 00:23:27 -0700

    Revamp parseArgs
    Better syntax using object {key:flag} mapping, improved syntax for passing multiple flags for one key using array of flags as value, support for double flags in positionalArgs, double flags are now correctly handled
    Fix bug caused by array reference mishandling when parsing fields

--- src/bugTestBot.js
+++ src/bugTestBot.js
@@ -12,6 +12,7 @@ const parseTime = require(`${process.cwd()}/util/parseTime.js`);
 const printTimePretty = require(`${process.cwd()}/util/printTimePretty.js`);
 const selfDeleteReply = require(`${process.cwd()}/util/selfDeleteReply.js`);
 const authorReply = require(`${process.cwd()}/util/authorReply.js`);
+const arrayEquals = require(`${process.cwd()}/util/arrayEquals.js`);
 
 //adds timestamps to log outputs
 function fixLogs()
@@ -381,19 +382,19 @@ client.on("message", async message => {
 		},
 		
 		"embed": async function() {
-			const pargs = parseArgs(args, ['author','author2','avatar','avatar2','avatar3','color','color2','thumbnail','thumbnail2','title','title2','description','description2','url','url2','url3','footer','footer2','footerAvatar','footerAvatar2','footerAvatar3','timestamp','timestamp2','image','image2'], ['a','-author','p','-author-avatar','-author-picture','c','-color','b','-thumbnail','t','-title','d','-description','u','-url','-link','f','-footer','o','-footer-avatar','-footer-picture','s','-timestamp','i','-image']);
-			const author = pargs.author || pargs.author2;
-			const avatar = pargs.avatar || pargs.avatar2 || pargs.avatar3;
-			const color = pargs.color || pargs.color2;
-			const thumbnail = pargs.thumbnail || pargs.thumbnail2;
-			const title = pargs.title || pargs.title2;
-			const description = pargs.description || pargs.description2;
-			const url = pargs.url || pargs.url2 || pargs.url3;
-			const footer = pargs.footer || pargs.footer2;
-			const footerAvatar = pargs.footerAvatar || pargs.footerAvatar2 || pargs.footerAvatar3;
-			const timestamp = pargs.timestamp || pargs.timestamp2;
-			const image = pargs.image || pargs.image2;
-			let rargs = pargs.args;
+			const pargs = parseArgs(args, {'author':['a', '-author'], 'avatar':['p', '-author-avatar', '-author-picture'], 'color':['c', '-color'], 'thumbnail':['b', '-thumbnail'], 'title':['t', '-title'], 'description':['d', '-description'], 'url':['u', '-url', '-link'], 'footer':['f', '-footer'], 'footerAvatar':['o', '-footer-avatar', '-footer-picture', '--footer-icon'], 'timestamp':['s', '-timestamp'], 'image':['i', '-image']});
+			const author = pargs.author;
+			const avatar = pargs.avatar;
+			const color = pargs.color;
+			const thumbnail = pargs.thumbnail;
+			const title = pargs.title;
+			const description = pargs.description;
+			const url = pargs.url;
+			const footer = pargs.footer;
+			const footerAvatar = pargs.footerAvatar;
+			const timestamp = pargs.timestamp;
+			const image = pargs.image;
+			let rargs = [...pargs.args];
 			const embed = new Discord.RichEmbed();
 			if(author) embed.setAuthor(author,avatar);
 			if(thumbnail) embed.setThumbnail(thumbnail);
@@ -406,7 +407,7 @@ client.on("message", async message => {
 			if(image) embed.setImage(image);
 			while(rargs != false)
 			{
-				const fargs = parseArgs(args, ['name','value','inline','inline2'], ['n','v','e','-inline']);
+				const fargs = parseArgs(rargs, {'name':'n','value':'v','inline':['e','-inline']});
 				if(fargs.name || fargs.value)
 				{
 					const name = fargs.name || '\u200B';
@@ -414,8 +415,8 @@ client.on("message", async message => {
 					const inline = fargs.inline || fargs.inline2;
 					embed.addField(name, value, inline);
 				}
-				if(rargs === fargs.args) break;
-				rargs = fargs.args;
+				if(arrayEquals(rargs,fargs.args)) break;
+				rargs = [...fargs.args];
 			}
 			message.channel.send(embed);
 		},
