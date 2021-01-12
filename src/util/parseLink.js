620b8003f20c3316d541e6cf5e718fd4f65526da
Walker Gray <55608093+wakfi@users.noreply.github.com>
2021-01-11 22:45:50 -0800

    Add parseLink Discord utility
    Fix issue in embed command where escaped links failed for url or image components

---
+++ src/util/parseLink.js
@@ -0,0 +1,12 @@
+const linkRegex = /^<?(https?:\/\/.+)>?$/;
+
+function parseLink(url)
+{
+	if(!url) return url;
+	const matches = linkRegex.exec(url);
+	if(!matches) return undefined;
+	const link = matches[1];
+	return link;
+}
+
+module.exports = parseLink;
